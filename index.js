dabfbbc5b8407fcb72065c07f04242f6da5dec4a
Victor Mamani Hualpa <victormamanihualpa@gmail.com>
2022-10-08 12:22:57 -0500

    feat: Create array with elements about me

---
+++ index.js
@@ -0,0 +1,8 @@
+const aboutMe = ['Victor',29,true,new Date('Jan 26 1993'),{
+    title:'The Picture of Dorian Gray',
+    author:'Oscar Wilde',
+    date: 1890,
+    url:'https://www.amazon.com/Picture-Dorian-Gray-Oscar-Wilde/dp/0141439572'
+}]
+
+console.log(aboutMe);
